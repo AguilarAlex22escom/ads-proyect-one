91df4d3c6388e418ee26b74f1f27ea02e92f8d0c
Miguel-Angel <miguel.angel.rgz.74@gmail.com>
2023-09-23 09:22:51 -0600

    Primer commit :D

---
+++ adsproyect/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5)qn126sechylx1-^l8&@_rh7b*(l&4l_2ypr6#xo*@rc@k-h&"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
